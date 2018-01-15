44edf2c46dfee6f42258829f21f62201f7753515
Jae Kyun Kim <kjaek@outlook.com>
2018-01-15 18:26:19 +1300

    Add project files.

---
+++ JaeHabitTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JaeHabitTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
